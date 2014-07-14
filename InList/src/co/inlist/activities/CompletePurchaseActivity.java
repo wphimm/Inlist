ce4870d9ef2f5aba27273cd1200f87bff996284e
himanshu dhakecha <himanshud@webplanex.com>
2014-07-14 11:42:15 +0530

    layout file added

--- InList/src/co/inlist/activities/CompletePurchaseActivity.java
+++ InList/src/co/inlist/activities/CompletePurchaseActivity.java
@@ -1,10 +1,12 @@
 package co.inlist.activities;
 
+import java.text.DecimalFormat;
+
 import org.json.JSONObject;
 
+import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
@@ -16,37 +18,78 @@ import co.inlist.interfaces.AsyncTaskCompleteListener;
 import co.inlist.util.Constant;
 import co.inlist.util.UtilInList;
 
+@SuppressLint("DefaultLocale")
 public class CompletePurchaseActivity extends Activity implements
 		ActionBar.OnNavigationListener, AsyncTaskCompleteListener {
 
 	public static CompletePurchaseActivity cpObj;
-	TextView txtPoints, txtTable, txtTotal;
+	TextView txtPoints, txtTable, txtTotal, txtDate, txtCardNum, txtCardName;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.complete_purchase);
-		
+
 		init();
-		
-		cpObj=this;
-		
-		String strHTML="<html><ul><li>I will arrive on-time before 12.30AM</li>" +
-				"<li>I will dress approprately for the venue</li>" +
-				"<li>I will arrive sober</li>" +
-				"<li>I understand this sale is final. Certain changes can be made in exchange for credit.</li></ul></html>";
+
+		cpObj = this;
+
+		String strHTML = "<html><body><ul><li>I will arrive on-time before 12.30AM</li>"
+				+ "<li>I will dress approprately for the venue</li>"
+				+ "<li>I will arrive sober</li>"
+				+ "<li>I understand this sale is final. Certain changes can be made in exchange for credit.</li></ul></body></html>";
 		txtPoints.setText(Html.fromHtml(strHTML));
-		
-		String strTable=""+InListApplication.getPricing().get(Integer.parseInt(UtilInList
-				.ReadSharePrefrence(CompletePurchaseActivity.this,
-						Constant.SHRED_PR.KEY_PRICE_POSITION)
-				.toString())).get("club_section_name");
-		txtTable.setText(""+strTable);
-		txtTotal.setText("$ "+UtilInList
-				.ReadSharePrefrence(CompletePurchaseActivity.this,
-						Constant.SHRED_PR.KEY_YOUR_MINIMUM)
-				.toString());
+
+		String strTable = ""
+				+ InListApplication
+						.getPricing()
+						.get(Integer.parseInt(UtilInList.ReadSharePrefrence(
+								CompletePurchaseActivity.this,
+								Constant.SHRED_PR.KEY_PRICE_POSITION)
+								.toString())).get("club_section_name");
+		txtTable.setText("" + strTable);
+		txtTotal.setText("$"
+				+ UtilInList.ReadSharePrefrence(CompletePurchaseActivity.this,
+						Constant.SHRED_PR.KEY_YOUR_MINIMUM).toString());
+
+		String strCardNum = ""
+				+ UtilInList.ReadSharePrefrence(CompletePurchaseActivity.this,
+						Constant.SHRED_PR.KEY_USER_CARD_NUMBER).toString();
+		txtCardNum.setText("" + strCardNum);
+
+		try {
+			String subString = strCardNum.substring(strCardNum.length() - 4,
+					strCardNum.length());
+			txtCardNum.setText("**** **** **** " + subString);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		txtCardName
+				.setText(""
+						+ UtilInList.ReadSharePrefrence(
+								CompletePurchaseActivity.this,
+								Constant.SHRED_PR.KEY_USER_CARD_HOLDER_NAME)
+								.toString());
+
+		String strYear = ""
+				+ UtilInList.ReadSharePrefrence(CompletePurchaseActivity.this,
+						Constant.SHRED_PR.KEY_USER_CARD_EXP_YEAR).toString();
+
+		try {
+			strYear = strYear.substring(strYear.length() - 2, strYear.length());
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		String strDate = new DecimalFormat("00").format(Integer
+				.parseInt(UtilInList.ReadSharePrefrence(
+						CompletePurchaseActivity.this,
+						Constant.SHRED_PR.KEY_USER_CARD_EXP_MONTH).toString()))
+				+ "/" + strYear;
+		txtDate.setText("" + strDate);
+
 	}
 
 	private void init() {
@@ -54,6 +97,9 @@ public class CompletePurchaseActivity extends Activity implements
 		txtPoints = (TextView) findViewById(R.id.txt_points);
 		txtTable = (TextView) findViewById(R.id.txt_table);
 		txtTotal = (TextView) findViewById(R.id.txt_total);
+		txtDate = (TextView) findViewById(R.id.txt_date);
+		txtCardNum = (TextView) findViewById(R.id.txt_card_num);
+		txtCardName = (TextView) findViewById(R.id.txt_card_name);
 	}
 
 	@Override
